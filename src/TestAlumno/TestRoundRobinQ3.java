93086b43565f9590167ed01f1b4d8980fd2e97e3
Ezequiel de la Fuente <ezequieldelafuente2001@gmail.com>
2020-10-20 23:56:51 -0300

    Commit inicial

---
+++ src/TestAlumno/TestRoundRobinQ3.java
@@ -0,0 +1,29 @@
+package TestAlumno;
+
+import Model.Prioridad;
+import Model.admProcesamiento;
+import Model.admTablaProcesos;
+
+public class TestRoundRobinQ3 {
+
+	public static void main(String[] args) {
+		admProcesamiento admP1 = new admProcesamiento(20, 38);
+
+		admP1.agregarProceso("P1", 1, 6, 5, 9, Prioridad.Alta);
+		admP1.agregarProceso("P2", 1, 6, 3, 5, Prioridad.Baja);
+		admP1.agregarProceso("P3", 2, 4, 6, 2, Prioridad.Media);
+		admP1.agregarProceso("P4", 3, 5, 4, 8, Prioridad.Baja);
+		admP1.agregarProceso("P5", 3, 6, 2, 4, Prioridad.Alta);
+		admP1.agregarProceso("P6", 4, 2, 1, 5, Prioridad.Media);
+
+		System.out.println("----------- Planificador RoundRobin q=1 -----------");
+		System.out.println(admP1.mostrarPlanificador(admP1.planificarRoundRobin(2)));
+		System.out.println(admP1.mostrarProcesos() + "\n-> hay 1 procesador" + "\n-> E/S Se realiza en paralelo\n");
+		
+		System.out.println("----------- Resultados RoundRobin q=1 -----------");
+		admTablaProcesos admTP1 = new admTablaProcesos(admP1);
+		System.out.println(admTP1.mostrarResultados(admP1.planificarRoundRobin(2)));
+
+	}
+
+}
